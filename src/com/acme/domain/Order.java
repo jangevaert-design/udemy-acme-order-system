8aa0e0f4695b19299831553f2ada8f95f790d87b
Jan Gevaert <jan.gevaert@gmail.com>
2020-11-09 09:42:06 -0700

    Added computing methods to order

--- src/com/acme/domain/Order.java
+++ src/com/acme/domain/Order.java
@@ -1,52 +1,64 @@
 package com.acme.domain;//import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Or;
 
 import com.acme.utils.MyDate;
-
 public class Order {
   MyDate orderDate;
   double orderAmount = 0.00;
   String customer;
   String product;
   int quantity;
-
-  public static double taxRate;
-
-  static {
-    taxRate = 0.05;
-  }
-
-  public Order(MyDate d, double amt, String c) {
-    orderDate = d;
-    orderAmount = amt;
-    customer = c;
-    product = "Anvil";
-    quantity = 1;
-
-  }
-
-  public Order(MyDate d, double amt, String c, String p, int q){
-    this.orderDate = d;
-    this.orderAmount = amt;
-    this.customer = c;
-    this.product = p;
-    this.quantity = q;
-  }
-
-  public double computeTax() {
-    System.out.println("The tax for this order is: " + orderAmount*Order.taxRate);
-    return orderAmount*Order.taxRate;
-  }
-
+  public static double taxRate = 0.05;
   public static void setTaxRate(double newRate) {
     taxRate = newRate;
   }
-
   public static void computeTaxOn(double anAmount) {
-    System.out.println("The tax for " +anAmount + " is: " + anAmount*Order.taxRate);
+    System.out.println("The tax for " + anAmount + " is: " +
+        anAmount
+            * Order.taxRate);
+  }
+  public Order(MyDate d, double amt, String c, String p, int q)
+  {
+    orderDate = d;
+    orderAmount = amt;
+    customer = c;
+    product = p;
+    quantity = q;
+  }
+  public String toString() {
+    return quantity + " ea. " + product + " for " + customer;
+  }
+  public double computeTax() {
+    System.out.println("The tax for this order is: " +
+        orderAmount
+            * Order.taxRate);
+    return orderAmount * Order.taxRate;
   }
 
-  public String toString(){
-    return quantity + " ea. " + product + " for " + customer;
+
+  public char jobSize() {
+    if (quantity <= 25) {
+      return 'S';
+    } else if (quantity <= 75) {
+      return 'M';
+    } else if (quantity <= 150) {
+      return 'L';
+    }
+    return 'X';
+  }
+
+  public double computeTotal() {
+    double total = orderAmount;
+    switch (jobSize()) {
+      case 'M': total = total - (orderAmount * 0.01);
+      break;
+      case 'L': total = total - (orderAmount * 0.02);
+      break;
+      case 'X': total = total - (orderAmount * 0/03);
+    }
+    if (orderAmount <= 1_500) {
+      total = total + computeTax();
+    }
+    return total;
   }
 
 }
